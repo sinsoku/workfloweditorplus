ea5c5cb90f3a8d58c32709aa5dfd3ddedf0f6992
sinsoku <sinsoku.listy@gmail.com>
2010-09-01 00:28:00 +0900

    Gridの同じタブをAdvanceに表示

--- 0.11/workfloweditor/htdocs/js/workfloweditor.js
+++ 0.11/workfloweditor/htdocs/js/workfloweditor.js
@@ -58,6 +58,11 @@ jQuery(document).ready(function(){
     context.initGrid("#workflowGrid");
     context.initStatusGrid("#workflowStatusGrid");
     
+    // display advance
+    context.updateModelByText("#workflowText");
+    context.initAdvance("#workflowAdvance");
+    context.initStatusAdvance("#workflowStatusAdvance");
+    
     // display tab layout
     var tabsElem = $("#workflowTabs > ul").tabs();
     var currentTab = "gridTab";
@@ -74,6 +79,9 @@ jQuery(document).ready(function(){
         } else if (ui.panel.id == "textTab") {
             context.updateModelByGrid("#workflowGrid");
             context.refreshText();
+        } else if (ui.panel.id == "advanceTab") {
+            context.updateModelByAdvance("#workflowAdvance");
+            context.refreshText();
         } else {
             // do nothing
         }
@@ -86,11 +94,14 @@ jQuery(document).ready(function(){
         {
             if (currentTab == "textTab") {
                 context.updateModelByText("#workflowText");
-                context.refreshGrid();            
+                context.refreshGrid();
             }
             
             context.updateModelByGrid("#workflowGrid");
             context.refreshText();
+            
+            // advance update
+            // advance refresh
         }
         catch(ex)
         {
@@ -136,6 +147,8 @@ workfloweditor.WorkflowContext = function() {
     this.textId;
     this.gridId;
     this.statusGridId;
+    this.advanceId;
+    this.statusAdvanceId;
 }
 
 /**
@@ -284,6 +297,83 @@ workfloweditor.WorkflowContext.prototype.updateModelByGrid = function(gridId) {
 }
 
 /**
+ * The workflow advance data is converted to the workflow model.
+ * 
+ * @param advanceId  workflow advance ID.
+ */
+workfloweditor.WorkflowContext.prototype.updateModelByAdvance = function(advanceId) {
+    this.advanceId = advanceId;
+    
+    var dataIds = jQuery(advanceId).getDataIDs();
+    if (!dataIds || dataIds.length <= 0) {
+        return;
+    }
+    
+    var model = [];
+    var status = this.status;
+    var rowNum = dataIds.length;
+    for (var index = 0; index < rowNum; index++) {
+        var rowData = jQuery(advanceId).getRowData(dataIds[index]);
+        var action = rowData["action"];
+        
+        if (!action) {
+            continue;
+        }
+        
+        var oldStatus = [];
+        for (col in rowData) {
+            var pos = col.indexOf(this.STATUS_PREFIX);
+            if ((pos == 0) && (rowData[col] == "Yes")) {
+                var tempStatus = col.substring(this.STATUS_PREFIX.length, col.length);
+                oldStatus.push(tempStatus);
+            }
+        }
+        
+        var defaultVal = rowNum - rowData["order"] + 1;
+        if (defaultVal < 0) {
+            defaultVal = 0;
+        }
+        
+        var operations;
+        for (ope in this.DEFAULT_OPERATIONS) {
+            if (rowData["operations"] == this.DEFAULT_OPERATIONS[ope]) {
+                operations = ope;
+                break;
+            }
+        }
+        
+        var permissions;
+        for (perm in this.DEFAULT_PERMISSIONS) {
+            if (rowData["permissions"] == this.DEFAULT_PERMISSIONS[perm]) {
+                permissions = perm;
+                break;
+            }
+        }
+        
+        workflow = {};
+        workflow["action"]      = action;
+        workflow["oldStatus"]   = oldStatus.join(",");
+        workflow["newStatus"]   = rowData["status"];
+        workflow["name"]        = rowData["name"];
+        workflow["default"]     = defaultVal;
+        workflow["operations"]  = operations;
+        workflow["permissions"] = permissions;
+        
+        model[action] = workflow;
+        
+        var newStatus = workflow["newStatus"];
+        if ((newStatus != "*") && ($.inArray(newStatus, status) < 0)) {
+            status.push(newStatus);
+        }
+    }
+    
+    workfloweditor.WorkflowContext.normalizeModel(model);
+    
+    this.model  = model;
+    this.status = status;
+}
+
+/**
  * Normalize the workflow model.
  * 
  * @param model  workflow model 
@@ -337,6 +427,17 @@ workfloweditor.WorkflowContext.prototype.refreshText = function() {
     $(this.textId).val(text);
 }
 
+/**
+ * Refresh the advance.
+ * If this method is called, the workflow model must be updated.
+ */
+workfloweditor.WorkflowContext.prototype.refreshAdvance = function() {
+    // remove the advance table and the advancve edit layout.
+    jQuery(this.advanceId).AdvanceUnload();
+    jQuery("#editmod" + this.advanceId.replace("#", "")).remove();
+    
+    this.initAdvance(this.advanceId);
+}
 
 /**
  * Initialize the workflow grid.
@@ -695,3 +796,326 @@ workfloweditor.WorkflowContext.prototype.createWorkflowText = function() {
     return text;
 }
 
+/**
+ * Initialize the workflow advance.
+ * 
+ * @param advanceId  advance element ID
+ */
+workfloweditor.WorkflowContext.prototype.initAdvance = function(advanceId) {
+    var isFirst;
+    if (!this.advanceId) {
+        isFirst = true;
+    } else {
+        isFirst = false;
+    }
+    
+    this.advanceId = advanceId;
+    
+    var colNames   = this.createAdvanceColNames();
+    var colModel   = this.createAdvanceColModel();
+    
+    jQuery(advanceId).jqGrid({
+        datatype    : "local",
+        height      : 200,
+        width       : 750,
+        colNames    : colNames,
+        colModel    : colModel,
+        shrinkToFit : false,
+        imgpath     : this.IMAGE_PATH,
+        editurl     : this.EDIT_URL
+    });
+    
+    var advanceData = this.createAdvanceData();
+    for(var index = 0; index < advanceData.length; index++) {
+        jQuery(advanceId).addRowData(index + 1, advanceData[index]);
+    }
+    
+    jQuery(advanceId).sortGrid("order", false);
+    
+    if (isFirst) {
+        var self = this;
+        var showAdvanceCol = function() {
+            jQuery(advanceId).showCol(self.HIDDEN_COL);
+        }
+        var hideAdvanceCol = function() {
+            $("#pData").hide();
+            $("#nData").hide();
+            jQuery(advanceId).hideCol(self.HIDDEN_COL);
+        }
+        
+        // add action setting
+        $(advanceId + "ItemAdd").click(function(){
+            jQuery(advanceId).editAdvanceRow(
+                "new",
+                {
+                 top               : 50,
+                 left              : 200,
+                 height            : 400,
+                 width             : 300,
+                 mtype             : "GET",
+                 closeAfterAdd     : true,
+                 reloadAfterSubmit : false,
+                 beforeInitData    : showAdvanceCol,
+                 afterShowForm     : hideAdvanceCol
+                }
+            );
+        });
+        
+        // modify action setting
+        $(advanceId + "ItemMod").click(function(){
+            var gr = jQuery(advanceId).getAdvanceParam("selrow");
+            if( gr != null ) {
+                jQuery(advanceId).editAdvanceRow(
+                    gr,
+                    {
+                     top               : 50,
+                     left              : 200,
+                     height            : 400,
+                     width             : 300,
+                     mtype             : "GET",
+                     closeAfterEdit    : true,
+                     reloadAfterSubmit : false,
+                     beforeInitData    : showAdvanceCol,
+                     afterShowForm     : hideAdvanceCol
+                    }
+                );
+            } else {
+                alert(_("Please select row."));
+            }
+        });
+        
+        // delete action setting
+        $(advanceId + "ItemDel").click(function(){
+            var gr = jQuery(advanceId).getAdvanceParam("selrow");
+            if( gr != null ) {
+                jQuery(advanceId).delAdvanceRow(
+                    gr,
+                    {
+                     top               : 50,
+                     left              : 200,
+                     mtype             : "GET",
+                     closeAfterEdit    : true,
+                     reloadAfterSubmit : false
+                    }
+                );
+            } else {
+                alert(_("Please select row."));
+            }
+        });
+        
+    } // isFirst end
+}
+
+/**
+ * Create the array of the advance column name.
+ * 
+ * @param colNames  the array of the advance column name
+ */
+workfloweditor.WorkflowContext.prototype.createAdvanceColNames = function() {
+    // for IE6
+    // Localization function isn't initialized in the local function.
+    if (!_) {
+        _ = workfloweditor.Localizer.getLocalizedString;
+    }
+    
+    var colNames = [_('action'), _('name'), _('operation'), _('permission'), _('order'), _('next status'), ''];
+    colNames = colNames.concat(this.status);
+    
+    return colNames;
+}
+
+/**
+ * Create the array of the advance column model.
+ * 
+ * @param colModel  the array of the advance column model
+ */
+workfloweditor.WorkflowContext.prototype.createAdvanceColModel = function() {
+    var statusValue = "*:*;";
+    for (var index = 0; index < this.status.length; index++) {
+        statusValue += this.status[index] + ":" + this.status[index] + ";";
+    }
+    statusValue = statusValue.substring(0, statusValue.length - 1);
+    
+    var opeValue = "";
+    for (ope in this.DEFAULT_OPERATIONS) {
+        opeValue += ope + ":" + this.DEFAULT_OPERATIONS[ope] + ";";
+    }
+    opeValue = opeValue.substring(0, opeValue.length - 1);
+    
+    var permValue = "";
+    for (perm in this.DEFAULT_PERMISSIONS) {
+        permValue += perm + ":" + this.DEFAULT_PERMISSIONS[perm] + ";";
+    }
+    permValue = permValue.substring(0, permValue.length - 1);
+    
+    var colModel = [
+        {name:'action',      index:'action',      width:75,  editable:true,  editrules:{required:true, edithidden:false}},
+        {name:'name',        index:'name',        width:100, editable:true,  editrules:{required:true}},
+        {name:'operations',  index:'operations',  width:100, editable:true,  edittype:"select", editoptions:{value:opeValue},  hidden:true},
+        {name:'permissions', index:'permissions', width:100, editable:true,  edittype:"select", editoptions:{value:permValue}, hidden:true},
+        {name:'order',       index:'order',       width:45,  editable:true,  align:"right", sorttype:"int", editrules:{integer:true}},
+        {name:'status',      index:'status',      width:100, editable:true,  edittype:"select", editoptions:{value:statusValue}},
+        {name:'blank',       index:'blank',       width:22,  editable:false, align:"center"}
+    ];
+    
+    for (var index = 0; index < this.status.length; index++) {
+        var statusColModel = {
+            name        : this.STATUS_PREFIX + this.status[index],
+            index       : this.STATUS_PREFIX + this.status[index],
+            width       : 75,
+            editable    : true,
+            align       : "center",
+            edittype    : "checkbox",
+            editoptions : {value:"Yes:No"}
+        };
+        
+        colModel.push(statusColModel);
+    }
+    
+    return colModel;
+}
+
+/**
+ * Create the advance data from the workflow model.
+ * 
+ * @return advance data
+ */
+workfloweditor.WorkflowContext.prototype.createAdvanceData = function() {
+    var model  = this.model;
+    var status = this.status;
+    
+    var modelSize = 0;
+    for (var key in model) {
+        modelSize++;
+    }
+    
+    var advanceData = [];
+    for (var action in model) {
+        var workflow = model[action];
+        var rowData = {};
+        
+        var operations;
+        if (this.DEFAULT_OPERATIONS[workflow["operations"]]) {
+            operations = this.DEFAULT_OPERATIONS[workflow["operations"]];
+        } else {
+            operations = "";
+        }
+        
+        var permissions;
+        if (this.DEFAULT_PERMISSIONS[workflow["permissions"]]) {
+            permissions = this.DEFAULT_PERMISSIONS[workflow["permissions"]];
+        } else {
+            permissions = "";
+        }
+        
+        rowData["action"]      = workflow["action"];
+        rowData["name"]        = workflow["name"];
+        rowData["status"]      = workflow["newStatus"];
+        rowData["operations"]  = operations;
+        rowData["permissions"] = permissions;
+        rowData["order"]       = modelSize - workflow["default"] + 1;
+        rowData["blank"]       = "<--";
+        
+        var oldStatus = workflow["oldStatus"] + ",";
+        for (var index = 0; index < status.length; index++) {
+            var tempStatus = status[index];
+            var match;
+            if ((oldStatus == "*,") || (oldStatus.indexOf(tempStatus + ",") >= 0)) {
+                match = "Yes";
+            } else {
+                match = "No";
+            }
+            rowData[this.STATUS_PREFIX + tempStatus] = match;
+        }
+        
+        advanceData.push(rowData);
+    }
+    
+    return advanceData;
+}
+
+/**
+ * Initialize the status advance.
+ * 
+ * @param advanceId  status advance element ID
+ */
+workfloweditor.WorkflowContext.prototype.initStatusAdvance = function(statusAdvanceId) {
+    var isFirst;
+    if (!this.statusAdvanceId) {
+        isFirst = true;
+    } else {
+        isFirst = false;
+    }
+    
+    this.statusAdvanceId = statusAdvanceId;
+    
+    jQuery(statusAdvanceId).hide();
+    
+    jQuery(statusAdvanceId).jqGrid({
+        datatype    : "local",
+        height      : 0,
+        width       : 0,
+        colNames    : [_("status")],
+        colModel    : [{
+                        name:'editableStatus', index:'status', width:100, editable:true,
+                        edittype:"textarea", editrules:{required:true}, editoptions: {rows:"10",cols:"15"},
+                        hidden:true
+                      }],
+        imgpath     : this.IMAGE_PATH,
+        editurl     : this.EDIT_URL,
+        loadui      : "disable"
+    });
+    
+    jQuery(statusAdvanceId).addRowData(1, {editableStatus : this.status.join(" ")});
+    
+    if (isFirst) {
+        var self = this;
+        var showAdvanceCol = function() {
+            jQuery(statusAdvanceId).showCol(["editableStatus"]);
+        }
+        var dataSplit = function() {
+            $("#pData").hide();
+            $("#nData").hide();
+            var statusValue = $("#editableStatus").val();
+            statusValue = statusValue.replace(/ /g, "\n");
+            $("#editableStatus").val(statusValue);
+            jQuery(statusAdvanceId).hideCol(["editableStatus"]);
+        }
+        var updateStatus = function() {
+            var statusValue = $("#editableStatus").val();
+            var statusArray = statusValue.split("\n");
+            
+            // update status
+            self.status = [];
+            for (var index = 0; index < statusArray.length; index++) {
+                var tempStatus = jQuery.trim(statusArray[index]);
+                if (tempStatus.length > 0) {
+                    self.status.push(tempStatus);
+                }
+            }
+            
+            self.updateModelByAdvance(self.advanceId);
+            self.refreshAdvance();
+        }
+        
+        // modify action setting
+        $(statusAdvanceId + "ItemMod").click(function(){
+            jQuery(statusAdvanceId).editAdvanceRow(
+                1,
+                {
+                 top               : 50,
+                 left              : 200,
+                 height            : 250,
+                 width             : 350,
+                 mtype             : "GET",
+                 closeAfterEdit    : true,
+                 reloadAfterSubmit : false,
+                 beforeInitData    : showAdvanceCol,
+                 afterShowForm     : dataSplit,
+                 onclickSubmit     : updateStatus
+                }
+            );
+        });
+    
+    } // isFirst end
+}
